13d974131f9e46fdb35eb2661c1d04d03c96acd2
bkc-tomi <bkc.tomi1979@gamil.com>
2021-02-26 15:06:21 +0900

    変数定義、再レンダー、要素返却

--- src/components/swipe/Swipe.js
+++ src/components/swipe/Swipe.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Tab from "../common/Tab";
 import Button from "../common/Button";
 import Slider from "../common/Slider";
-import GrayText from "../common/GrayText";
 import { SiteContext } from "../../App";
+import NoImage from "../../assets/img/noimage.png";
+import NoImage2 from "../../assets/img/noimage2.png";
 
 /**
  * Swipe
@@ -14,15 +15,84 @@ import { SiteContext } from "../../App";
 const Swipe = () => {
     // 変数定義 ===========================================================================
     const { state } = useContext(SiteContext);
+    // 画像情報
+    const [oldImg, setOldImg] = useState({ src: NoImage });
+    const [newImg, setNewImg] = useState({ src: NoImage2 });
+    // 表示サイズ
+    const [oldSize, setOldSize]   = useState({
+        width : 500,
+        height: 500,
+    });
+    const [newSize, setNewSize]   = useState({
+        width : 500,
+        height: 500,
+    });
+    // スワイプサイズ
+    const [swipeWidth, setSwipeWidth] = useState(0);
     // 機能リスト
     const list = [
-        {name: "2-Up", active: false},
+        {name: "2-Up" , active: false},
         {name: "Swipe", active: true},
         {name: "Onion", active: false},
-    ];
+    ]
 
     // 関数定義 ============================================================================
-    // スワイプ
+    // スワイプ ★未実装
+    const changeWidth = (num) => {
+        console.log(num);
+        const size = Math.round(num * 0.01 * newSize.width);
+        setSwipeWidth(size);
+    }
+
+    // 再レンダー =========================================================================
+    useEffect(() => {
+        // 古い画像取得
+        let oldWidth  = 0;
+        let oldHeight = 0;
+        if (state.old) {
+            setOldImg(state.old);
+            var tempWidth  = state.old.width;
+            var tempHeight = state.old.height;
+            if (tempWidth > tempHeight) {
+                oldWidth  = Math.round(tempWidth / tempWidth * 500);
+                oldHeight = Math.round(tempHeight / tempWidth * 500);
+            } else {
+                oldWidth  = Math.round(tempWidth / tempHeight * 500);
+                oldHeight = Math.round(tempHeight / tempHeight * 500);
+            }
+        }
+        // 新しい画像取得
+        let newWidth  = 0;
+        let newHeight = 0;
+        if (state.new) {
+            setNewImg(state.new);
+            tempWidth  = state.new.width;
+            tempHeight = state.new.height;
+            if (tempWidth > tempHeight) {
+                newWidth  = Math.round(tempWidth / tempWidth * 500);
+                newHeight = Math.round(tempHeight / tempWidth * 500);
+            } else {
+                newWidth  = Math.round(tempWidth / tempHeight * 500);
+                newHeight = Math.round(tempHeight / tempHeight * 500);
+            }
+        }
+        // サイズ更新
+        if (oldWidth * oldHeight > 0) {
+            setOldSize({ width: oldWidth, height: oldHeight});
+            setNewSize({ width: oldWidth, height: oldHeight});
+        }
+        if (newWidth * newHeight > 0) {
+            setOldSize({ width: newWidth, height: newHeight});
+            setNewSize({ width: newWidth, height: newHeight});
+        }
+        if (
+            oldWidth * oldHeight > 0 &&
+            newWidth * newHeight > 0 &&
+            oldWidth * oldHeight !== newWidth * newHeight
+        ) {
+            alert("サイズの異なる画像が選択されたので、古い方の画像を新しい方の画像のサイズに変更しています。元の画像からアスペクト比が崩れている可能性があります。");
+        }
+    }, [newImg, oldImg, state.old, state.new]);
     // 要素返却 ============================================================================
     return (
         <div className="grid grid-row-6 bg-gray-800 rounded-lg shadow-md w-full h-full">
@@ -31,29 +101,21 @@ const Swipe = () => {
             </div>
             <div className="row-span-4 justify-start items-center">
             <div className="relative w-full h-full">
-                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                        <img src={ state.old ? state.old.src : "" } alt="old" />
+                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center" >
+                        <img id="swipe-old-img" src={ oldImg.src } alt="old" style={ oldSize } />
                     </div>
-                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                        <img src={ state.new ? state.new.src : "" } alt="new" />
+                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center" >
+                        <div id="swipe-new-img" style={{ "width": swipeWidth }}>
+                            <img src={ newImg.src } alt="new" style={ newSize } />
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="flex flex-col justify-end items-center">
-                <div className="grid grid-cols-2 w-full mb-3">
-                    <div className="flex flex-col justify-center items-center text-gray-400">
-                        <GrayText>width: { state.old ? state.old.width : "" }</GrayText>
-                        <GrayText>height: { state.old ? state.old.height : "" }</GrayText>
-                    </div>
-                    <div className="flex flex-col justify-center items-center text-gray-400">
-                        <GrayText>width: { state.new ? state.new.width : "" }</GrayText>
-                        <GrayText>height: { state.new ? state.new.height : "" }</GrayText>
-                    </div>
-                </div>
                 <div className="bg-gray-900 rounded-lg w-full grid grid-cols-6">
                     <div className="col-span-2"></div>
                     <div className="col-span-2 flex flex-col justify-center items-center">
-                        <Slider id="swipe" size={400}>100</Slider>
+                        <Slider id="swipe" size={400} func={ (num) => changeWidth(num) }>100</Slider>
                     </div>
                     <div className="col-span-2">
                         <Button cls="py-2 px-4 m-3 ml-24">SAVE</Button>
