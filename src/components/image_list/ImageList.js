9b830e5f498f9aba7b28c1e9da6a542ed8dadc23
bkc-tomi <bkc.tomi1979@gamil.com>
2021-02-26 14:46:38 +0900

    機能完成。save機能まだ

--- src/components/image_list/ImageList.js
+++ src/components/image_list/ImageList.js
@@ -135,7 +135,13 @@ error message: ${ image.errMsg }
                 <div className="justify-start items-center overflow-hidden">
                     <LimeText cls="text-2xl font-extrabold">{ props.type }</LimeText>
                 </div>
-                <div className="col-span-5 justify-center items-center overflow-hidden">
+                <div className="col-span-5 flex flex-row justify-center items-center overflow-hidden">
+                    <div className="text-gray-400 mr-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+                            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+                        </svg>
+                    </div>
                     <GrayText cls="text-3xl my-5">Drug and drop image here.</GrayText>
                 </div>
             </div>
--- src/components/onion/Onion.js
+++ src/components/onion/Onion.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Tab from "../common/Tab";
 import Button from "../common/Button";
-import GrayText from "../common/GrayText";
 import Slider from "../common/Slider";
 import { SiteContext } from "../../App";
+import NoImage from "../../assets/img/noimage.png";
 
 /**
  * Onion
@@ -14,9 +14,19 @@ import { SiteContext } from "../../App";
 const Onion = () => {
     // 変数定義 ===========================================================================
     const { state } = useContext(SiteContext);
+    const [oldImg, setOldImg] = useState({ src: NoImage });
+    const [newImg, setNewImg] = useState({ src: NoImage });
+    const [oldSize, setOldSize]   = useState({
+        width : 500,
+        height: 500,
+    });
+    const [newSize, setNewSize]   = useState({
+        width : 500,
+        height: 500,
+    });
     // 機能リスト
     const list = [
-        {name: "2-Up", active: false},
+        {name: "2-Up" , active: false},
         {name: "Swipe", active: false},
         {name: "Onion", active: true},
     ]
@@ -27,6 +37,56 @@ const Onion = () => {
         const elem = document.getElementById("onion-new-img");
         elem.style.opacity = num / 100;
     }
+    
+    // 再レンダー =========================================================================
+    useEffect(() => {
+        // 古い画像取得
+        let oldWidth  = 0;
+        let oldHeight = 0;
+        if (state.old) {
+            setOldImg(state.old);
+            var tempWidth  = state.old.width;
+            var tempHeight = state.old.height;
+            if (tempWidth > tempHeight) {
+                oldWidth  = Math.round(tempWidth / tempWidth * 500);
+                oldHeight = Math.round(tempHeight / tempWidth * 500);
+            } else {
+                oldWidth  = Math.round(tempWidth / tempHeight * 500);
+                oldHeight = Math.round(tempHeight / tempHeight * 500);
+            }
+        }
+        // 新しい画像取得
+        let newWidth  = 0;
+        let newHeight = 0;
+        if (state.new) {
+            setNewImg(state.new);
+            tempWidth  = state.new.width;
+            tempHeight = state.new.height;
+            if (tempWidth > tempHeight) {
+                newWidth  = Math.round(tempWidth / tempWidth * 500);
+                newHeight = Math.round(tempHeight / tempWidth * 500);
+            } else {
+                newWidth  = Math.round(tempWidth / tempHeight * 500);
+                newHeight = Math.round(tempHeight / tempHeight * 500);
+            }
+        }
+        // サイズ更新
+        if (oldWidth * oldHeight > 0) {
+            setOldSize({ width: oldWidth, height: oldHeight});
+            setNewSize({ width: oldWidth, height: oldHeight});
+        }
+        if (newWidth * newHeight > 0) {
+            setOldSize({ width: newWidth, height: newHeight});
+            setNewSize({ width: newWidth, height: newHeight});
+        }
+        if (
+            oldWidth * oldHeight > 0 &&
+            newWidth * newHeight > 0 &&
+            oldWidth * oldHeight != newWidth * newHeight
+        ) {
+            alert("サイズの異なる画像が選択されたので、古い方の画像を新しい方の画像のサイズに変更しています。元の画像からアスペクト比が崩れている可能性があります。");
+        }
+    }, [newImg, oldImg, state.old, state.new]);
 
     // 要素返却 ===========================================================================
     return (
@@ -37,34 +97,29 @@ const Onion = () => {
             <div className="row-span-4 justify-start items-center">
                 <div className="relative w-full h-full">
                     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                        <img src={ state.old ? state.old.src : "" } alt="old" />
+                        <img src={ oldImg.src } alt="old" style={ oldSize } />
                     </div>
                     <div 
                         id="onion-new-img"
                         className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0"
                     >
-                        <img src={ state.new ? state.new.src : "" } alt="new" />
+                        <img src={ newImg.src } alt="new" style={ newSize } />
                     </div>
                 </div>
             </div>
             <div className="flex flex-col justify-end items-center">
-                <div className="grid grid-cols-2 w-full mb-3">
-                    <div className="flex flex-col justify-center items-center text-gray-400">
-                        <GrayText>width: { state.old ? state.old.width : "" } - height: { state.old ? state.old.height : "" }</GrayText>
-                        <GrayText>update_at: { state.old ? state.old.update_at.toLocaleString("ja") : "" }</GrayText>
-                    </div>
-                    <div className="flex flex-col justify-center items-center text-gray-400">
-                        <GrayText>width: { state.new ? state.new.width : "" } - height: { state.new ? state.new.height : "" }</GrayText>
-                        <GrayText>update_at: { state.new ? state.new.update_at.toLocaleString("ja") : "" }</GrayText>
-                    </div>
-                </div>
                 <div className="bg-gray-900 rounded-lg w-full grid grid-cols-6">
                     <div className="col-span-2"></div>
                     <div className="col-span-2 flex flex-col justify-center items-center">
                         <Slider id="onion" func={ (num) => changeOpacity(num) } size="500px">100</Slider>
                     </div>
                     <div className="col-span-2">
-                        <Button cls="py-2 px-4 m-3 ml-24">SAVE</Button>
+                        <Button cls="py-2 px-4 m-3 ml-24 flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+                                <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
+                            </svg>
+                            <p className="ml-2">SAVE</p>
+                        </Button>
                     </div>
                 </div>
             </div>
